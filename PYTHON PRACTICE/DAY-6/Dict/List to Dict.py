8c5cd1024dd1d531b0151b4348bbe68d219203d4
DineshK1307 <105618164+DineshK1307@users.noreply.github.com>
2022-05-16 21:44:54 +0530

    Add files via upload

---
+++ PYTHON PRACTICE/DAY-6/Dict/List to Dict.py
@@ -0,0 +1,4 @@
+l1 = ['5 trophies','4 trophies','2 trophies']
+l2 = ['MI','CSK','KKR']
+dict_list = dict(zip(l1,l2))  #Used zip which will return iterable data to the caller
+print(f"list to dict : {dict_list}")
